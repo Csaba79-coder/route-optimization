0fe9200bafb5c95849f86ec11ac1a7aed109b447
Csaba Vadász <csabavadasz79@gmail.com>
2024-09-15 14:31:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Do not forget to add the downloaded map here:
+
+![img.png](img.png)
+
+link:
+
+https://download.geofabrik.de/europe.html (download the map of the country you want to use)
---
+++ src/test/java/com/csaba79coder/routeoptimization/RouteOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.routeoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
