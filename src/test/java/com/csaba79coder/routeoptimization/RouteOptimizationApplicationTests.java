02622c20d670ebe8232aebbcc5c059fccc48959f
Csaba Vadász <csabavadasz79@gmail.com>
2024-09-15 17:42:15 +0200

    add api key to application.properties for test

--- src/test/java/com/csaba79coder/routeoptimization/RouteOptimizationApplicationTests.java
+++ src/test/java/com/csaba79coder/routeoptimization/RouteOptimizationApplicationTests.java
@@ -2,8 +2,10 @@ package com.csaba79coder.routeoptimization;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "${api.token}")
 class RouteOptimizationApplicationTests {
 
     @Test
